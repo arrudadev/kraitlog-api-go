9ab3c3f0ec9fcc9567973e2db3dc6c8d078350a5
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-08-16 12:33:31 -0300

    add user api handler

---
+++ internal/infrastructure/api/user/user_handler.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/arrudadev/kraitlog-api/internal/application/user/dto"
+	usecase "github.com/arrudadev/kraitlog-api/internal/application/user/usecase/create_user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	createUserUseCase usecase.CreateUserUseCase
+}
+
+func NewUserHandler(createUserUseCase usecase.CreateUserUseCase) *UserHandler {
+	return &UserHandler{createUserUseCase: createUserUseCase}
+}
+
+func (handler *UserHandler) CreateUser(context *gin.Context) {
+	var createUserDTO *dto.CreateUserDTO
+
+	if err := context.ShouldBindJSON(&createUserDTO); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := handler.createUserUseCase.Execute(createUserDTO)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"user": user})
+}
