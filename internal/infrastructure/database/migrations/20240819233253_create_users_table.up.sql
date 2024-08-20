8bd7ffbbd31be8a4369d81d675763932638cbe55
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-08-19 23:42:28 -0300

    add create_users_table migration

---
+++ internal/infrastructure/database/migrations/20240819233253_create_users_table.up.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+  id UUID PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION function_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION function_updated_at_column();
